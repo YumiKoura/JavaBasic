39ead0751d47f7696d12bc8af497d8dd9122c95a
DESKTOP-0CS8D75\likeit_student <yumi.koura.likeit@gmail.com>
2018-10-17 18:46:06 +0900

    Practice２-10まで完了

--- Practice/src/practice01/PTra01_02.java
+++ Practice/src/practice01/PTra01_02.java
@@ -10,11 +10,13 @@ public class PTra01_02 {
 	public static void main(String[] args) {
 
 		// ★ 12～14行目をコメントにしてください。
+		/*ここをコメントにしてください
+		ここをコメントにしてください
+		ここをコメントにしてください*/
 
+		//  ★「Hello, world」と出力（コマンドプロンプトに表示）してください//
 
-		//  ★「Hello, world」と出力（コマンドプロンプトに表示）してください
-		System.out.println("Hello, world");
-
+		System.out.println("「Hello, world」");
 
 	}
 }
